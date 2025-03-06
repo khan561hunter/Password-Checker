2e2dcd8557e58edff6db90ae712e13243d7041f7
khan561hunter <waniakhan561@gmail.com>
2025-03-06 21:37:53 +0500

    changes

--- the-app/app.py
+++ the-app/app.py
@@ -4,16 +4,35 @@ st.set_page_config(page_title="Password Checker",layout="centered")
 
 st.title("Password Checker")
 
+count = 0
+
 password = st.text_input("Enter the password",type="password")
 
 if st.button("Check"):
-    if len(password) < 8:
-        st.error("Password should be at least 8 characters long.")
-    elif not any(char.isupper() for char in password):
-        st.error("Password should contain at least one uppercase letter.")
-    elif not any(char.islower() for char in password):
-        st.error("Password should contain at least one lowercase letter.")
-    elif not any(char.isdigit() for char in password):
-        st.error("Password should contain at least one digit.")
+    if len(password) >= 8:
+        count += 1
+    else : 
+        st.error("Password must be at least 8 characters long")
+    if any(char.isupper() for char in password):
+        count += 1
     else:
+        st.error("Password must contain at least one uppercase letter")
+    if any(char.islower() for char in password):
+        count += 1
+    else:
+        st.error("Password must contain at least one uppercase letter")
+    if any(char.isdigit() for char in password):
+        count += 1
+    else:
+        st.error("Password must contain at least one number")
+    if any(char in "!@#$%^&*()" for char in password):
+        count += 1
+    else:
+        st.error("Password must contain at least one special character")
+    if count == 5 :
         st.success("Password is strong")
+    elif count == 4 or count == 3 :
+        st.warning("Password is moderate")
+    else:
+        st.error("Password is weak")
+st.write("The total count is : " ,count)
