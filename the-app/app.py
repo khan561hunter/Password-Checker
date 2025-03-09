36032650c1c683bc42226022a27a7d2ce7e70c47
khan561hunter <waniakhan561@gmail.com>
2025-03-10 02:27:08 +0500

    updated

--- the-app/app.py
+++ the-app/app.py
@@ -11,22 +11,27 @@ password = st.text_input("Enter the password",type="password")
 if st.button("Check"):
     if len(password) >= 8:
         count += 1
+        st.success("length is good")
     else : 
         st.error("Password must be at least 8 characters long")
     if any(char.isupper() for char in password):
         count += 1
+        st.success("Password contains atleast one uppercase letter")
     else:
         st.error("Password must contain at least one uppercase letter")
     if any(char.islower() for char in password):
         count += 1
+        st.success("Password contains atleast one lowercase letter")
     else:
         st.error("Password must contain at least one uppercase letter")
     if any(char.isdigit() for char in password):
         count += 1
+        st.success("Password contains atleast one number")
     else:
         st.error("Password must contain at least one number")
     if any(char in "!@#$%^&*()" for char in password):
         count += 1
+        st.success("Password contains atleast one special character")
     else:
         st.error("Password must contain at least one special character")
     if count == 5 :
@@ -34,5 +39,5 @@ if st.button("Check"):
     elif count == 4 or count == 3 :
         st.warning("Password is moderate")
     else:
-        st.error("Password is weak")
+        st.warning("Password is weak")
 st.write("The total count is : " ,count)
